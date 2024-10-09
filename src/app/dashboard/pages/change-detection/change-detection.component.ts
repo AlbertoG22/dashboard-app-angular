e20920fb2e11ef04251a6fd8dce7a59c54b8d497
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-10-08 21:16:11 -0600

    ChangeDetection - OnPush

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, TitleComponent ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './change-detection.component.html',
-  styles: ``
 })
 export default class ChangeDetectionComponent {
+  public currentFramework = computed(
+    () => `Change detection - ${ this.frameworkAsSignal().name }`
+  );
 
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016
+  });
+
+  public frameworkAsProperty ={
+    name: 'Angular',
+    releaseDate: 2016
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // this.frameworkAsProperty.name = 'React';
+      this.frameworkAsSignal.update( val => ({
+        ...val,
+        name: 'React'
+      }));
+
+      console.log('hecho');
+    }, 3000);
+  }
 }
