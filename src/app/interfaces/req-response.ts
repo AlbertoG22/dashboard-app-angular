e855c00d2ef5eea3f03ff8221457d4ada2fe6775
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-10-17 14:22:46 -0600

    services with signals

---
+++ src/app/interfaces/req-response.ts
@@ -0,0 +1,21 @@
+export interface UsersResponse {
+  page:         number;
+  per_page:     number;
+  total:        number;
+  total_pages:  number;
+  data:         User[];
+  support:      Support;
+}
+
+export interface User {
+  id:         number;
+  first_name: string;
+  email:      string;
+  last_name:  string;
+  avatar:     string;
+}
+
+export interface Support {
+  url: string;
+  text: string;
+}
