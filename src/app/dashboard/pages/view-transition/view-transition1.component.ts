7e9b268121ba0c69a23c8a962b29a73191217ed1
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-10-17 13:51:40 -0600

    view transition animation pt2

--- src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -14,10 +14,13 @@ import { TitleComponent } from '@shared/title/title.component';
         alt="Picsum"
         width="200"
         height="300"
+        style="view-transition-name: hero1;"
       />
 
-      <div class="bg-blue-950 w-56 h-56">
-
+      <div 
+        class="bg-blue-950 w-56 h-56"
+        style="view-transition-name: hero2;"
+        >
       </div>
     </section>
   `
