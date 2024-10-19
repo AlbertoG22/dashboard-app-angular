3c6e4bb75d2fffa0f5cb825a20c45658b1b027f8
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-10-18 20:36:07 -0600

    from observable to signal - toSignal()

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TitleComponent } from '@shared/title/title.component';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
+import { UsersService } from '@services/users.service';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styles: ``
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title title="User" />
+
+    @if( user() ) {
+      <section>
+        <img
+          [srcset]="user()!.avatar"
+          [alt]="user()!.first_name"
+        />
+
+        <div>
+          <h3>{{ user()?.first_name }} {{ user()?.last_name }}</h3>
+          <p>{{ user()?.email }}</p>
+        </div>
+      </section>
+    } @else {
+      <p>Cargando información...</p>
+    }
+  `
 })
 export default class UserComponent {
+
+  private route = inject( ActivatedRoute );
+  private usersService = inject( UsersService );
+
+  public user = toSignal(
+    this.route.params.pipe(
+      switchMap( ({ id }) => this.usersService.getUserById( id ))
+    )
+  );
+
+  constructor() {
+    // console.log(this.route.params.subscribe(params => {
+    //   console.log({params});
+    // }));
+  }
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,7 +1,7 @@
 import { computed, inject, Injectable, signal } from '@angular/core';
-import { User, UsersResponse } from '../interfaces/req-response';
+import type { User, UserResponse, UsersResponse } from '../interfaces/req-response';
 import { HttpClient } from '@angular/common/http';
-import { delay } from 'rxjs';
+import { delay, map } from 'rxjs';
 
 interface State {
   users: User[];
@@ -25,12 +25,20 @@ export class UsersService {
 
   constructor() { 
     this.http.get<UsersResponse>('https://reqres.in/api/users')
-    .pipe( delay(1500) )
-    .subscribe( res => {
+      .pipe( delay(1500) )
+      .subscribe( res => {
         this.#state.set({
           loading: false,
           users: res.data,
         });
       })
+  }
+
+  getUserById( id: string ) {
+    return this.http.get<UserResponse>(`https://reqres.in/api/users/${ id }`)
+      .pipe(
+        delay(1500),
+        map( resp => resp.data )
+      )
   }
 }
