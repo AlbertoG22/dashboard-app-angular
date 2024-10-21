ec88b941f1ca52f155e390bdcea26f0c1212b722
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-10-21 13:43:48 -0600

    using computed properties

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TitleComponent } from '@shared/title/title.component';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -10,7 +10,7 @@ import { UsersService } from '@services/users.service';
   standalone: true,
   imports: [CommonModule, TitleComponent],
   template: `
-    <app-title title="User" />
+    <app-title [title]="titleLabel()" />
 
     @if( user() ) {
       <section>
@@ -39,6 +39,14 @@ export default class UserComponent {
       switchMap( ({ id }) => this.usersService.getUserById( id ))
     )
   );
+  
+  public titleLabel = computed( () => {
+    if( this.user() ) {
+      return `Información del usuario: ${this.user()?.first_name} ${this.user()?.last_name}`;
+    }
+
+    return 'Información del usuario';
+  });
 
   constructor() {
     // console.log(this.route.params.subscribe(params => {
